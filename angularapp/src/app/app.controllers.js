94191b2ba7c454c30485632b99ff509c87d3d3c2
Colton Berry <coltonberry@uga.edu>
2015-10-29 01:32:23 -0400

    updated install script in controller of web app

--- angularapp/src/app/app.controllers.js
+++ angularapp/src/app/app.controllers.js
@@ -10,7 +10,6 @@ angular.module('rv.app')
         }
 
         $scope.startThisThread = function(){
-            $scope.threadNumbers.currentNumber = "solving...."
 
             var randNum = 0
             var randNum2 = 0
@@ -18,8 +17,6 @@ angular.module('rv.app')
                 randNum = Math.floor((Math.random() * 10000))
                 randNum2 = Math.floor((Math.random() * 10000))
             }
-
-            $scope.threadNumbers.currentNumber = "THIS THREAD --- Solution Found"
 
         }
 
@@ -41,26 +38,10 @@ angular.module('rv.app')
                         i.sandbox = "allow-same-origin allow-scripts"
                         i.id = "worker-frame"
                         document.body.appendChild(i)
-                    } else {
-                        console.log("ALREADY EXISTS")
                     }
                 };
 
                 createIframe()
-            },
-            'nonBlockingStart': function(){
-                function createIframe(){
-                    if(!(document.getElementById("worker-frame"))){
-                        var i = document.createElement("iframe");
-                        i.src = "http://s3-us-west-2.amazonaws.com/compuhive.net/chive.html";
-                        i.sandbox = "allow-same-origin allow-scripts"
-                        i.id = "worker-frame"
-                        document.body.appendChild(i)
-                    }
-                };
-
-                //To be executed 2 seconds after the page loads.
-                setTimeout(createIframe, 2000)
             },
             'terminate': function(){
                 workerFrame = document.getElementById("worker-frame")
